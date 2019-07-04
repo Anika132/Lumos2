35e85dd525869630b6472ad8c4f6c982aab3fd58
Anika <anikasharma132@gmail.com>
2019-07-04 19:16:57 +0530

    Updates

---
+++ precache-manifest.f81da475410d9d93495919fd38721d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7095468af27b5dbf751f55799f47a8",
+    "url": "/Lumos2/index.html"
+  },
+  {
+    "revision": "ae0a998e234bb60b4ffe",
+    "url": "/Lumos2/static/css/main.4adc360a.chunk.css"
+  },
+  {
+    "revision": "84ae854f4f71e78ea4f5",
+    "url": "/Lumos2/static/js/2.07ce042d.chunk.js"
+  },
+  {
+    "revision": "ae0a998e234bb60b4ffe",
+    "url": "/Lumos2/static/js/main.65572a58.chunk.js"
+  },
+  {
+    "revision": "207218f4379c56a6d74e",
+    "url": "/Lumos2/static/js/runtime~main.48e7adb1.js"
+  },
+  {
+    "revision": "4abded6e4e416025bbc21b01c8458d90",
+    "url": "/Lumos2/static/media/logo.4abded6e.png"
+  }
+]);
